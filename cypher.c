b1542a64b32124dc2f0c1b92fd98aa9cc479e380
Joel INFELD <jinfeld@e1z3r1p23.42.us.org>
2017-02-23 18:49:39 -0800

    dafuq

---
+++ cypher.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cypher.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinfeld <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/09 11:56:38 by jinfeld           #+#    #+#             */
+/*   Updated: 2017/02/09 18:50:48 by jinfeld          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+static void	ft_bzero(void *s, size_t n)
+{
+	unsigned char *res;
+
+	res = s;
+	while (n--)
+	{
+		*res++ = 0;
+	}
+}
+static void	*ft_memalloc(size_t size)
+{
+	void *res;
+
+	res = malloc(size);
+	if (!res)
+		return (NULL);
+	ft_bzero(res, size);
+	return (res);
+}
+
+static void	cypher(char *str, char *bp)
+{
+	int hashi;
+	int j;
+	int i;
+
+	i = 0;
+	j = 0;
+	hashi = -1;
+	while (str[i])
+	{
+		if (str[i] == '#')
+		{
+			if(i - hashi == 1 || hashi == -1)
+				bp[j++] = '#';
+			if(i - hashi == 3)
+				bp[j++] = '3';
+			if(i - hashi == 4)
+				bp[j++] = '-';
+			if(i - hashi == 5)
+				bp[j++] = 'n';
+			if(i - hashi == 6)
+				bp[j++] = '+';
+			hashi = i;
+		}
+		i++;
+	}
+	bp[j] = '\0';
+}
+static int	weed(char *str)
+{
+	int w;
+	int count;
+	int i;
+
+	count = 0;
+	w = 0;
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == '#')
+			count++;
+		if (str[i] == '\n')
+		{
+			if (count > w)
+				w = count;
+			count = 0;
+		}
+		i++;
+	}
+	return (w);
+}
+tet		maketet(char *str, int i)
+{
+	tet		ret;
+	int		width;
+	
+	width = 0;
+	//if(!(ret = (tet)malloc(sizeof(tet))))
+	//	return (ret);
+	if(!(ret.bp = ft_memalloc(5)))
+		return (ret);
+	cypher(str, ret.bp);
+	ret.w = weed(str);
+	ret.alpha = 'A' + i;
+	ret.use = 0;
+	return (ret);
+}
--- fillit.c
+++ fillit.c
@@ -6,10 +6,20 @@
 /*   By: jinfeld <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/09 19:04:13 by jinfeld           #+#    #+#             */
-/*   Updated: 2017/02/23 16:18:02 by jinfeld          ###   ########.fr       */
+/*   Updated: 2017/02/23 18:44:56 by jinfeld          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fillit.h"
+static int ft_sqrt(int nb, int cap)
+{
+	if (nb = 0)
+		return (0);
+	if (nb * nb != cap)
+		return (ft_sqrt(nb - 1, cap));
+	if (nb * nb == cap)
+		return (nb);
+	else return (0);
+}
 
 static int codebreak(char **sq, char *bp, int x, int y)
 {
@@ -31,9 +41,13 @@ static int codebreak(char **sq, char *bp, int x, int y)
 		codebreak(sq, ++bp, x + 1, y - 2);
 	sq[x][y] = '.';
 }
+
 static int nearestsq(int nb)
 {
-	
+	if (!ft_sqrt(nb, nb))
+		ft_sqrt(nb + 1, nb + 1);
+	else
+		return(ft_sqrt(nb, nb));
 }
 
 static void	blanksq(char **sq, int size)
@@ -43,15 +57,20 @@ static void	blanksq(char **sq, int size)
 	int j;
 
 	sz = nearestsq(size);
+	sq = (char**)(ft_memalloc(sz))
 	i = 0;
-	while (i < size)
+	while (i < sz)
+		sq[i++] = (char *)(ft_memalloc(sz + 1));
+	sq[i] = NULL;
+	i = 0;
+	while (i < size && s[i])
 	{
 		j = 0;
 		while (j < size)
 		{
 			sq[i][j++] = '.';
 		}
-		sq[i][j] = '\n';
+		sq[i][j] = '\0';
 		i++;
 	}
 }
@@ -70,7 +89,7 @@ static int	fit(char **sq, tet 	*p, int nm)
 		while (sq[x][y])
 		{
 			if (sq[x][y] == '.' && codebreak(sq, *p, x, y))
-		{
+			{
 				if (fit(sq, p, nm))
 					return (1);
 			}
@@ -85,7 +104,7 @@ char	**fillit(tet *p, int nm, int sz)
 {
 	char	**sq;
 	blanksq(sq, sz);
-		if(!fit(sq, p, nm))
-			fillit(p, nm, sz + 1)
+	if(!fit(sq, p, nm))
+		fillit(p, nm, sz + 1)
 	return(sq);
 }
